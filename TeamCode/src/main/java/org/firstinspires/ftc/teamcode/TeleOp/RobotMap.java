f09490adbfd897c543749cee4fa6acefa7908c79
Paulina <avilaosoriopaulina@gmail.com>
2025-07-22 19:54:42 -0400

    codigo chasis y extension

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/RobotMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/RobotMap.java
@@ -2,36 +2,33 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class RobotMap {
-    public DcMotorEx leftExtensionMotor = null;
-    public DcMotorEx rightExtensionMotor = null;
 
+
+    public DcMotorEx leftExtend;
+    public DcMotorEx rightExtend;
 
 
     public RobotMap(HardwareMap Init)
     {
-       //GLOBAL
-        try {
-            leftExtensionMotor = Init.get(DcMotorEx.class, "leftExtend"); // Usa tus nombres de config
-            rightExtensionMotor = Init.get(DcMotorEx.class, "rightExtend"); // Usa tus nombres de config
+        //EXTENDERS
+        leftExtend=Init.get(DcMotorEx.class,"leftExtend");
+        leftExtend.setDirection(DcMotor.Direction.REVERSE);
+        leftExtend.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftExtend.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftExtend.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-            // La dirección y el reseteo de encoders se manejarán en ExtendController
-            // Pero puedes establecer el ZeroPowerBehavior aquí si quieres
-            leftExtensionMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            rightExtensionMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-            // NO pongas RUN_TO_POSITION o STOP_AND_RESET_ENCODER aquí si el controlador lo hace.
-            // Solo asegura que los motores estén configurados para usar encoders.
-            leftExtensionMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            rightExtensionMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightExtend=Init.get(DcMotorEx.class,"rightExtend");
+        rightExtend.setDirection(DcMotor.Direction.FORWARD);
+        rightExtend.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightExtend.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightExtend.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
 
-        } catch (Exception e) {
-            System.err.println("Error en RobotMap al inicializar motores de extensión: " + e.getMessage());
-            // Marcar que hubo un error o lanzar una excepción puede ser útil
-        }
+
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/controllers/ExtendController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/controllers/ExtendController.java
@@ -1,126 +1,79 @@
 package org.firstinspires.ftc.teamcode.TeleOp.controllers;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.TeleOp.RobotMap;
 
+@Config
 public class ExtendController {
+    public enum liftStatus {
+        INIT,
+        POWEROFF,
+        COLLECT,
 
-    public enum ExtendPosition {
-        CERRADO,    // Retracted
-        MEDIO,      // Mid extension
-        ABIERTO,    // Fully extended
-        APAGADO     // STOP_AND_RESET_ENCODER
     }
 
-    public static final int RETRACTED_TICKS = 0;
-    public static final int MID_EXTENDED_TICKS = 500;
-    public static final int FULLY_EXTENDED_TICKS = 1615;
+    public static liftStatus currentStatus = liftStatus.INIT;
+    public liftStatus previousStatus = null;
 
-    public static final double EXTEND_POWER = 0.85;
+    public static DcMotorEx rightExtend = null;
+    public static DcMotorEx leftExtend = null;
 
-    private RobotMap robot;
-    private DcMotorEx leftExtensionMotor;
-    private DcMotorEx rightExtensionMotor;
+    public int init_position = 0;
+    public int collect_position = 1400;
+    public static int medium_position = 1150;
 
-    public static ExtendPosition currentState = ExtendPosition.CERRADO;
-    private ExtendPosition previousState = null;
+    public static int rightLiftSpeed = 2500;
+    public static int leftLiftSpeed = 2500;
 
-    public ExtendController(RobotMap robotMap) {
-        this.robot = robotMap;
+    public int currentPosition = init_position;
 
-        try {
-            leftExtensionMotor = robot.leftExtensionMotor;
-            rightExtensionMotor = robot.rightExtensionMotor;
-
-            if (leftExtensionMotor == null || rightExtensionMotor == null)
-                throw new IllegalArgumentException("Motores no configurados");
-
-            leftExtensionMotor.setDirection(DcMotor.Direction.REVERSE);
-            rightExtensionMotor.setDirection(DcMotor.Direction.FORWARD);
-
-            leftExtensionMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            rightExtensionMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-            stopAndResetEncoders();
-
-            setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        } catch (Exception e) {
-            System.err.println("Error inicializando ExtendController: " + e.getMessage());
-        }
+    public ExtendController(RobotMap robot) {
+        rightExtend = robot.rightExtend;
+        leftExtend = robot.leftExtend;
     }
 
-    public void update() {
-        if (currentState != previousState) {
-            previousState = currentState;
+    public void update(int target) {
+        // Si el estado ha cambiado
+        if (currentStatus != previousStatus) {
+            previousStatus = currentStatus;
 
-            switch (currentState) {
-                case CERRADO:
-                    setTargetPosition(RETRACTED_TICKS);
-                    setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    setPower(EXTEND_POWER);
+            switch (currentStatus) {
+
+                case INIT:
+                    rightExtend.setTargetPosition(init_position);
+                    rightExtend.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+                    rightExtend.setVelocity(rightLiftSpeed);
+
+                    leftExtend.setTargetPosition(init_position);
+                    leftExtend.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+                    leftExtend.setVelocity(leftLiftSpeed);
+
+                    currentPosition = init_position;
                     break;
 
-                case MEDIO:
-                    setTargetPosition(MID_EXTENDED_TICKS);
-                    setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    setPower(EXTEND_POWER);
+                case POWEROFF:
+                    rightExtend.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    rightExtend.setPower(0);
+                    leftExtend.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    leftExtend.setPower(0);
                     break;
 
-                case ABIERTO:
-                    setTargetPosition(FULLY_EXTENDED_TICKS);
-                    setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    setPower(EXTEND_POWER);
+                case  COLLECT:
+                    rightExtend.setTargetPosition(collect_position);
+                    rightExtend.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+                    rightExtend.setVelocity(rightLiftSpeed);
+
+                   leftExtend.setTargetPosition(collect_position);
+                   leftExtend.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+                   leftExtend.setVelocity(leftLiftSpeed);
+
+                    currentPosition = collect_position;
                     break;
 
-                case APAGADO:
-                    stopMotors();
-                    stopAndResetEncoders();
-                    break;
             }
         }
-    }
-
-    // --- Utility Methods ---
-    public void goToPosition(ExtendPosition position) {
-        currentState = position;
-    }
-
-    private void setTargetPosition(int ticks) {
-        if (leftExtensionMotor != null) leftExtensionMotor.setTargetPosition(ticks);
-        if (rightExtensionMotor != null) rightExtensionMotor.setTargetPosition(ticks);
-    }
-
-    private void setMode(DcMotor.RunMode mode) {
-        if (leftExtensionMotor != null) leftExtensionMotor.setMode(mode);
-        if (rightExtensionMotor != null) rightExtensionMotor.setMode(mode);
-    }
-
-    private void setPower(double power) {
-        if (leftExtensionMotor != null) leftExtensionMotor.setPower(Math.abs(power));
-        if (rightExtensionMotor != null) rightExtensionMotor.setPower(Math.abs(power));
-    }
-
-    public void stopAndResetEncoders() {
-        if (leftExtensionMotor != null) {
-            leftExtensionMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        }
-        if (rightExtensionMotor != null) {
-            rightExtensionMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        }
-    }
-    public void stopMotors() {
-        setPower(0);
-        setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    }
-
-    public boolean areMotorsBusy() {
-        return (leftExtensionMotor != null && leftExtensionMotor.isBusy()) ||
-                (rightExtensionMotor != null && rightExtensionMotor.isBusy());
-    }
-
-    public ExtendPosition getCurrentState() {
-        return currentState;
     }
 }
